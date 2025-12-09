0ab3b8dab73001a00c7fa2ebcb55952331728cab
altanemredemirci <altan_emre_1989@hotmail.com>
2025-12-09 21:39:27 +0300

    While Finished

--- 09_Donguler/Program.cs
+++ 09_Donguler/Program.cs
@@ -131,37 +131,170 @@
             //}
 
             #region Kullanıcı 0 sayısını girene kadar alınan her sayıyı toplayan ve 0 girdiğin sonucu ekrana yazdıran while loop
-           
+
+            //int toplam = 0;
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Sayı:");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //    if (sayi == 0)
+            //    {
+            //        //Console.WriteLine("Toplam:" + toplam);
+            //        break;
+            //    }
+
+            //        toplam += sayi;
+
+            //}
+            //Console.WriteLine("Toplam:" + toplam);
+
+
+            #endregion
+
+            #region 0-500 aralığında tek sayıların toplayan ve ekrana yazan while döngüsü 
+
+            //int i = 0;
+            //int toplam = 0;
+            //while (i < 500)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        toplam += i; //toplam=toplam+i;
+            //    }
+
+            //    i++;
+            //}
+
+            //Console.WriteLine("Toplam:"+toplam);
+
+
+
+
+            //2.Yol
+            //int i = 1;
+            //int toplam = 0;
+            //while (i < 500)
+            //{
+            //    toplam += i;
+            //    i += 2;
+            //}
+
+            //Console.WriteLine("Toplam:" + toplam);
+
+            #endregion
+
+
+            #region Kullanıcıdan 0 girilene kadar pozitif sayılar girmesi isteyin
+            //0 girilene kadar girilen pozitif sayılar toplansın ve ortalaması hesaplansın.
+            // Kullanıcı 0 girdiğinde döngü bitsin ve toplam,ortalama ekrana yazdırılsın.           
+            // ** kullanıcı negatif sayı girerse "lütfen Pozitif Sayı Giriniz!!" uyarısını verilsin.
+
+            //int toplam = 0;
+            //int adet = 0;
+            //while (true)
+            //{
+            //    Console.WriteLine("Sayı giriniz:");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //    if (sayi == 0)
+            //    {
+            //        break;
+            //    }
+            //    else if (sayi < 0)
+            //    {
+            //        Console.WriteLine("Pozitif Sayı giriniz");
+            //    }
+            //    else
+            //    {
+            //        toplam += sayi;
+            //        adet++;
+            //    }
+            //}
+            //Console.WriteLine("Toplam="+toplam);
+            //Console.WriteLine("Ortalama="+(toplam/adet));
+
+
+
+            #endregion
+
+
+            #endregion
+
+            #region FOR LOOP
+
+            //1-10 aralığındaki sayıları ekrana yazdırın
+
+            //int i = 1;
+            //while (i < 11)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //for(int i=1; i<11; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            #region Kullanıcıdan başlangıç ve bitiş değerlerini alınız. bu aralıktaki sayıları ekrana yazdırınız
+
+            //Console.WriteLine("Başlangıç:");
+            //int basla = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Bitiş:");
+            //int bitis = Convert.ToInt32(Console.ReadLine());
+
+
+            //int toplam = 0;
+
+            //for (int i = basla; i < bitis; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    toplam += i;
+            //}
+            //Console.WriteLine(toplam);
+
+            #endregion
+
+
+            #region Kullanıcıdan alınan bir sayının faktöriyeli hesaplayan for döngüsünü yazınız.
+
+            //5 => 1*2*3*4*5 => 120
+
+            //Console.WriteLine("Sayı:");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //int fakt = 1;
+            //for (int i = 1; i <= sayi; i++)
+            //{
+            //    fakt *= i;
+            //}
+
+            //Console.WriteLine("Faktöriyel:"+fakt);
+
+            #endregion
+
+            #region 1-500 arasındaki sayıların içinden 13 katlarının kaç tane olduğunu ve toplamlarını hesaplayınız
+
             int toplam = 0;
+            int adet = 0;
 
-            while (true)
+            for (int i = 1; i < 500; i++)
             {
-                Console.WriteLine("Sayı:");
-                int sayi = Convert.ToInt32(Console.ReadLine());
-
-                if (sayi == 0)
+                if (i % 13 == 0)
                 {
-                    //Console.WriteLine("Toplam:" + toplam);
-                    break;
-                }
-                else
-                {
-                    toplam += sayi;
+                    toplam += i;
+                    adet++;
                 }
             }
-            Console.WriteLine("Toplam:" + toplam);
-
-
-            #endregion
-
-
-
-
-
+            Console.WriteLine("Toplam:"+toplam);
+            Console.WriteLine("Adet:"+adet);
 
             #endregion
-
-
+            #endregion
 
 
         }
