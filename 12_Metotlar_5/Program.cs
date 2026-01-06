ad2c66d0aeb348e88a92ea5372ab8d93b3fd0a49
altanemredemirci <altan_emre_1989@hotmail.com>
2026-01-06 21:32:28 +0300

    Değer Döndüren Metotlar

---
+++ 12_Metotlar_5/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _12_Metotlar_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DEĞER DÖNDÜREN METOTLAR
+            // Bir metot gerçekleştirdiği işlem sonucunu return kelimesi ile çağrıldığı yere taşıyor ise değer döndüren metot denir.
+
+            //int sonuc = Toplama();
+
+            //Console.WriteLine("3.sayı:");
+            //int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(sonuc+sayi3);
+
+
+            //SORU: kullanıcıdan alınacak 2 ürün fiyatından pahalı olana %30 indirim uygulayan metot ve 3.ürün almak isterse 3.ürüne %50 indirim uygulayarak metot ile kodlayınız
+
+            Indirim2();
+
+
+        }
+
+        static int Toplama()
+        {
+            Console.WriteLine("1.sayı:");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2.sayı:");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = sayi1 + sayi2;
+
+            return toplam;
+        }
+
+        static void Indirim2()
+        {
+            Console.WriteLine("1.Ürün Fiyatı:");
+            double fiyat1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2.Ürün Fiyatı:");
+            double fiyat2 = Convert.ToDouble(Console.ReadLine());
+
+            if (fiyat1 > fiyat2)
+            {
+                fiyat1 = fiyat1 * 0.7;
+            }
+            else
+            {
+                fiyat2 = fiyat2 * 0.7;
+            }
+
+            double fiyat3 = Indirim3();
+
+            Console.WriteLine("Ödemeniz:"+(fiyat1+fiyat2+fiyat3));
+
+        }
+
+        static double Indirim3()
+        {
+            Console.WriteLine("3.ürün ister misiniz?E/H");
+            string cevap = Console.ReadLine();
+            if (cevap == "E")
+            {
+                Console.WriteLine("3.Ürün Fiyatı:");
+                double urun3 = Convert.ToDouble(Console.ReadLine());
+
+                return urun3 / 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ 12_Metotlar_6/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace _12_Metotlar_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Bir şirkette çalışan maaş hesaplama programı yazmanız isteniyor.
+            
+            Görev 1: Aşağıdaki metotları yazınız
+            a) double MaasHesapla(double saatUcreti, int calisilanSaat)
+
+            Parametrelerle gelen saatlik ücret ve çalışılan saati çarparak toplam maaşı hesaplayıp döndüren metot yazın.
+
+
+            b) double VergiHesapla(double maas)
+
+            Maaşın %20'ini vergi olarak hesaplayıp döndüren metot yazın.
+
+
+
+            c) double NetMaasHesapla(double brutMaas, double vergi)
+
+            Brüt maaştan vergiyi çıkararak net maaşı hesaplayıp döndüren metot yazın.
+
+
+            d) string MaasDurumu(double netMaas)
+
+            Net maaş 15000'den fazla ise "Yüksek Gelir"
+            Net maaş 8000-15000 arası ise "Orta Gelir"
+            Net maaş 8000'den az ise "Düşük Gelir"
+            string olarak döndüren metot yazın (if-else kullanın)
+
+
+
+            Görev 2: Main metotunda aşağıdaki işlemleri yapınız
+            Kullanıcıdan saatlik ücret ve çalışılan saat bilgilerini alın
+            MaasHesapla metodunu çağırarak brüt maaşı hesaplayın
+            VergiHesapla metodunu çağırarak vergi miktarını bulun
+            NetMaasHesapla metodunu çağırarak net maaşı hesaplayın
+            MaasDurumu metodunu çağırarak gelir durumunu öğrenin
+            Tüm sonuçları ekrana yazdırın
+             
+            */
+
+            Console.WriteLine("Saat Ücreti:");
+            double saatUcreti = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Çalışma Saati:");
+            int calismaSaati = Convert.ToInt32(Console.ReadLine());
+
+            double maas = MaasHesapla(saatUcreti, calismaSaati);
+
+            double vergi = VergiHesapla(maas);
+
+            double netMaas = NetMaasHesapla(maas, vergi);
+
+            MaasDurumu(netMaas);
+
+        }
+
+        static double MaasHesapla(double saatUcreti, int calisilanSaat)
+        {
+            //Hesaplanan maaş metodun çağrıldığı yere taşınır.
+            return saatUcreti * calisilanSaat;
+        }
+
+        static double VergiHesapla(double maas)
+        {
+            return maas * 0.20;
+        }
+
+        static double NetMaasHesapla(double brutMaas, double vergi)
+        {
+            return brutMaas - vergi;
+        }
+
+        static void MaasDurumu(double netMaas)
+        {
+            if (netMaas > 15000)
+            {
+                Console.WriteLine("Yüksek Gelir");
+            }
+            else if(netMaas<=15000 && netMaas > 8000)
+            {
+                Console.WriteLine("Orta Gelir");
+            }
+            else
+            {
+                Console.WriteLine("Düşük Gelir");
+            }
+        }
+    }
+}
