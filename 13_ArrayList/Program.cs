cd1fc1e9307014307b85b66af37000b49002bd3a
altanemredemirci <altan_emre_1989@hotmail.com>
2026-01-15 21:34:22 +0300

    ArrayList and SortedList

--- 13_ArrayList/Program.cs
+++ 13_ArrayList/Program.cs
@@ -28,7 +28,7 @@ namespace _13_ArrayList
 
             //ArrayList Ekrana Yazdırma
             //var:variable anlamına gelir. Bütün veri tiplerini kapsar. 
-            //foreach (var item in arrayList)
+            //foreach (var item in arrayList2)
             //{
             //    Console.WriteLine(item);
             //}
@@ -71,7 +71,7 @@ namespace _13_ArrayList
 
             //*** COUNT ve CAPACITY
 
-            //ArrayList arrayList = new ArrayList() { 11, 23, 34, 45,56,7,8,9,0 };
+            //ArrayList arrayList = new ArrayList() { 11, 23, 34, 45,1 };
 
             //Console.WriteLine("Eleman Sayısı:"+arrayList.Count); //Eleman Sayısı
 
@@ -80,9 +80,147 @@ namespace _13_ArrayList
 
             //*** CLEAR()
 
-            ArrayList arrayList = new ArrayList() { 11, 23, 34, 45, 56, 7, 8, 9, 0 };
+            //ArrayList arrayList = new ArrayList() { 11, 23, 34, 45, 56, 7, 8, 9, 0 };
 
-            arrayList.Clear();//bütün değerleri siler.
+            //arrayList.Clear();//bütün değerleri siler.
+
+            #region Kullanıcıdan aile birey sayısını öğrenin daha sonra aile birey isimlerini alarak arrayliste atın ve ekrana yazdırınız.
+
+            //ArrayList aile = new ArrayList();
+
+            //Console.WriteLine("Kaç kişiyiz?");
+            //int kisiSayisi = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i < kisiSayisi; i++)
+            //{
+            //    Console.WriteLine("Aile Birey Adı:");
+            //    string ad = Console.ReadLine();
+
+            //    aile.Add(ad);
+            //}
+
+            //foreach (string item in aile)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            //ArrayList arrayList = new ArrayList() { "İstanbul", "Ankara", "Çanakkale", "Kars", "Balıkesir", "Kastamonu" };
+
+            //arrayList.Remove("Ankara");
+
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //arrayList.Sort();
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //arrayList.Reverse();
+
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //arrayList.RemoveAt(2);
+
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //ArrayList arrayList = new ArrayList() { "İstanbul", "Ankara", "Çanakkale", "Kars", "Balıkesir", "Kastamonu" };
+
+            //ArrayList arrayList2 = arrayList;
+
+            //ArrayList arrayList2 = (ArrayList)arrayList.Clone();
+
+            //arrayList.Add("İzmir");
+
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("----------------");
+
+            //foreach (var item in arrayList2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+
+            //ArrayList arrayList = new ArrayList() { "İstanbul", "Ankara", "Çanakkale", "Kars", "Balıkesir", "Kastamonu" };
+
+            //ArrayList arrayList2 = new ArrayList() { 1, 2, 3 };
+
+            //arrayList2.AddRange(arrayList);
+
+            //foreach (var item in arrayList2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //Varlık Kontrolü Yapar.
+            //if (arrayList.Contains("ankara")) 
+            //{
+            //    Console.WriteLine("Var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yok");
+            //}
+
+
+            //Değerin Index numarasını verir.
+            //int index = arrayList.IndexOf("Ankara");
+            //Console.WriteLine(index);
+
+
+            #region Kullanıcıdan sayı istenilecek girilen değer sayı ise bir arraylist e atılacak. Girilen değer sayı değilde 'çık' ifadesi olursa arraylist toplanarak ekrana sonuç yazılacak
+
+            ArrayList arrayList = new ArrayList();
+
+            while (true)
+            {
+                Console.WriteLine("Sayı giriniz:");
+                string deger = Console.ReadLine();
+                int sayi;
+
+                if (deger == "çık")
+                {
+                    break;
+                }
+                else if(int.TryParse(deger,out sayi))
+                {
+                    arrayList.Add(sayi);
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı Değer Girişi");
+                }
+            }
+            int toplam = 0;
+            foreach (int item in arrayList)
+            {
+                toplam += item;
+            }
+
+            Console.WriteLine("Toplam:"+toplam);
+            #endregion
 
         }
     }
---
+++ 14_SortedList/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+
+namespace _14_SortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SortedList: Key:Value mantığı ile çalışır. Sözlük gibi kullanımı vardır. Key üzerinden valur değerine erişilebilir ve aynı sözlük gibi A-Z ye sıralıdır.
+            //SortedList<key,value>
+
+            SortedList sozluk = new SortedList()
+            {
+                {"Bugün","Today"},
+                {"Yarın","Tomorrow" },
+                {"Hava","Weather" },
+                {"Gökyüzü","Sky" },
+                {"Karlı","Snowy" },
+                {"Güneşli","Sunny" },
+                {"Bulutlu","Cloudy" },
+                {"Açık","Clear" },
+                {"Kapalı","Cloudy" },
+                //{"Üç",3 },
+                //{4,"Four" }
+            };
+
+            //Console.WriteLine(sozluk["Bugün"]); //Anahtar değer üzerinden çalışır.
+            //Console.WriteLine(sozluk["Today"]); //Today diye bir key değer olmadığı için boş döner.
+
+
+            //foreach (DictionaryEntry item in sozluk)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (string item in sozluk.Keys)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (string item in sozluk.Values)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            // Eleman Ekleme
+            sozluk.Add("Yağmur", "Rain");         
+
+            sozluk["Siyah"] = "Black"; //Varsa günceller, yoksa ekler.
+            sozluk["Siyah"] = "DarkBlack";
+
+            foreach (DictionaryEntry item in sozluk)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
